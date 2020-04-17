d58168c8685db74403feb3158977755ebf72ec49
Raines <rainesdemint@gmail.com>
2020-04-17 13:22:42 -0700

    Added QmsDocXml.Tests project and wrote EffectiveDate test

--- QmsHero/QDoc/Docs/DocConfig.cs
+++ QmsHero/QDoc/Docs/DocConfig.cs
@@ -16,21 +16,6 @@ namespace QDoc.Docs
             this.Initialize();
         }
 
-        #region Header
-        public int HeaderFooterSection { get => headerFooterSection; set => headerFooterSection = value; }
-        public int EffectiveDateRow { get => effectiveDateRow; set => effectiveDateRow = value; }
-        public int EffectiveDateCol { get => effectiveDateCol; set => effectiveDateCol = value; }
-        public int RevisionCol { get => revisionCol; set => revisionCol = value; }
-        public int RevisionRow { get => revisionRow; set => revisionRow = value; }
-        public int LogoRow { get => logoRow; set => logoRow = value; }
-        public int LogoCol { get => logoCol; set => logoCol = value; }
-        public string EffectiveDateText { get => effectiveDateText; set => effectiveDateText = value; }
-        public string RevisionText { get => revisionText; set => revisionText = value; }
-        public Regex EffectiveDateRegex { 
-            get => effectiveDateRegex; 
-            set => effectiveDateRegex = value; }
-
-        #endregion
         public virtual void Initialize()
         {
             throw new NotImplementedException();
---
+++ QmsHero/QmsDocXml.Tests/Docs/Word/Properties/EffectiveDateTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QDoc.Test;
+using QmsDoc.Docs.Word;
+using QmsDocXml.Docs.Word.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QmsDocXml.Docs.Word.Properties.Tests
+{
+    [TestClass()]
+    public class EffectiveDateTests
+    {
+        [TestMethod()]
+        public void ReadTest()
+        {
+            string actual = "2019-11-05";
+            string effDate = "2020-20-20";
+            var fixture = new FixtureUtil();
+            var doc = new WordDoc(fixture.WordSample);
+            var prop = new EffectiveDate();
+            string result = doc.Inspect(prop).Value;
+            Assert.AreEqual(result, actual);
+        }
+    }
+}
