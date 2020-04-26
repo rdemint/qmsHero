988725bdad70c5e8f6e3d16dbc5575da4bfc0f17
Raines <rainesdemint@gmail.com>
2020-04-25 18:17:08 -0700

    Logo tests are passing for Word and Excel! QmsDocXml Library.

--- QmsHero/QmsDocXml.Tests/LogoTests.cs
+++ QmsHero/QmsDocXml.Tests/LogoTests.cs
@@ -10,63 +10,44 @@ namespace QmsDocXml.Tests
     public class LogoTests
     {
         [TestMethod]
-        public void ReadTest()
+        public void ReadExcelTest()
         {
-            var fixture = new Fixture();
-            
-            //word
-            var doc = new WordDoc(fixture.WordSampleCopy);
-            var result = (string)doc.Inspect(new Logo()).State;
-            Assert.AreEqual("GT Medical Logo II.jpg", result);
 
-            //excel
+            var fixture = new Fixture();
             var xl = new ExcelDoc(fixture.ExcelSampleCopy);
-            result = (string)xl.Inspect(new Logo()).State;
+            var result = (string)xl.Inspect(new Logo()).State;
             Assert.AreEqual("GT Medical Logo II", result);
         }
 
         [TestMethod]
-        public void WriteTest()
+        public void ReadWordTest()
+        {
+            var fixture = new Fixture();
+            var doc = new WordDoc(fixture.WordSampleCopy);
+            var result = (string)doc.Inspect(new Logo()).State;
+            Assert.AreEqual("GT Medical Logo II.jpg", result);
+        }
+
+        [TestMethod]
+        public void WriteWordTest()
         {
             var fixture = new Fixture();
 
-            //word
-            //var doc = new WordDoc(fixture.WordSampleCopy);
-            //var initial = (string)doc.Inspect(new Logo()).State;
-            //Assert.AreEqual("GT Medical Logo II.jpg", initial);
+            var logo = new Logo(fixture.LogoSampleJpgCopy.FullName);
+            var doc = new WordDoc(fixture.WordSampleCopy);
+            doc.Process(logo);
+            var result = (string)doc.Inspect(new Logo()).State;
+            Assert.AreEqual(fixture.LogoSampleJpgCopy.Name, result);
+        }
 
-            //var logo = new Logo(fixture.LogoSampleJpgCopy.FullName);
-            //doc.Process(logo);
-            //var result = (string)doc.Inspect(new Logo()).State;
-            //Assert.AreEqual(fixture.LogoSampleJpgCopy.Name, result);
-
-            //excel
+        [TestMethod]
+        public void WriteExcelTest()
+        {
+            var fixture = new Fixture();
             var xl = new ExcelDoc(fixture.ExcelSampleCopy);
-            xl.Process(new Logo(fixture.LogoSampleJpgCopy));
+            xl.Process(new Logo(fixture.LogoSampleJpgCopy.FullName));
             var xlResult = (string)xl.Inspect(new Logo()).State;
             Assert.AreEqual(fixture.LogoSampleJpgCopy.Name, xlResult);
-        }
-
-        [TestMethod]
-        public void ImageStyleRegexTest()
-        {
-            string myS = "position:absolute;margin-left:0;margin-top:0;width:79.5pt;height:28.5pt;   z-index:1";
-            Match widthMatch = Regex.Match(myS, @"width:.*pt");
-            Match heightMatch = Regex.Match(myS, @"height:.*pt");
-            Assert.AreEqual(widthMatch.Success, true);
-            Assert.AreEqual(heightMatch.Success, true);
-
-        }
-
-        [TestMethod]
-        public void StyleParseTest()
-        {
-            string hS = "height:23.5pt";
-            string wS = "width:32.0pt";
-            string h = "23.5";
-            double dh = 23.5;
-            Assert.AreEqual(double.Parse(h), dh);
-            
         }
     }
 }
