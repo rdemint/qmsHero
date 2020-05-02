6e0ad2e99ad0a648e8e1cbdec05a06f621928354
Raines <rainesdemint@gmail.com>
2020-05-01 17:56:38 -0700

    ResultsView now showing list of processed documents, which one have errors, and gives a link to the file which opens its.

--- QmsHero/QmsHero/ViewModel/CustomProcessingViewModel.cs
+++ QmsHero/QmsHero/ViewModel/CustomProcessingViewModel.cs
@@ -12,6 +12,7 @@ using QmsDoc.Core;
 using QDoc.Core;
 using QmsDocXml.Common.PropertyGroups;
 using System.IO;
+using System.Diagnostics;
 
 namespace QmsHero.ViewModel
 {
@@ -32,12 +33,17 @@ namespace QmsHero.ViewModel
             this.Manager = SimpleIoc.Default.GetInstance<DocManager>();
             this.resultsViewModel = SimpleIoc.Default.GetInstance<ResultsViewModel>();
             this.HeaderPropertyGroup = new HeaderPropertyGroup();
-            this.referenceDirPath = "C:\\Users\\raine\\Desktop\\qmsProcessing\\Test\\Reference";
-            this.processingDirPath = "C:\\Users\\raine\\Desktop\\qmsProcessing\\Test\\Processing";
+            
             this.processFilesCommand = new RelayCommand(
                         () => ProcessFiles(),
                         () => ProcessingDirIsValid() && ReferenceDirIsValid()
                         );
+            //
+            this.HeaderPropertyGroup.HeaderLogo.State = "C:\\Users\\raine\\Desktop\\qmsProcessing\\Test\\Reference\\qaladder_logo.jpg";
+            this.referenceDirPath = "C:\\Users\\raine\\Desktop\\qmsProcessing\\Test\\Reference";
+            this.processingDirPath = "C:\\Users\\raine\\Desktop\\qmsProcessing\\Test\\Processing";
+            this.ProcessFiles();
+            //
         }
 
         public RelayCommand ProcessFilesCommand {
@@ -57,6 +63,7 @@ namespace QmsHero.ViewModel
             int errorCount = docCollection.Where(doc => doc.PropertyResultCollection.Any(result => result.IsSuccess == false)).Count();
             resultsViewModel.DocCollection = docCollection;
             MessageBox.Show($"Finished Processing the files. {docCollection.Count} files were processed and {errorCount} files had errors.");
+
         }
 
         private bool ProcessingDirIsValid()
--- QmsHero/QmsHero/ViewModel/ResultsViewModel.cs
+++ QmsHero/QmsHero/ViewModel/ResultsViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Ioc;
 using QDoc.Docs;
 using QmsHero.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +15,18 @@ namespace QmsHero.ViewModel
     public class ResultsViewModel : ViewModelBase
     {
         DocCollection docCollection;
+        RelayCommand<string> openFileCommand;
+        string name;
 
         public ResultsViewModel() : base()
+
         {
+
             this.DocCollection = SimpleIoc.Default.GetInstance<ProcessingResultsStore>().DocCollection;
+            this.openFileCommand = new RelayCommand<string>(
+                file => Process.Start(file)
+                );
+            this.Name = "ResultsView";
         }
 
         public DocCollection DocCollection
@@ -33,5 +43,8 @@ namespace QmsHero.ViewModel
             //    DocCollection.Add()
             //}
         }
+
+        public RelayCommand<string> OpenFileCommand { get => openFileCommand; set => openFileCommand = value; }
+        public string Name { get => name; set => name = value; }
     }
 }
